767d95b86ac568ac5a9141d37a2ee20f7277841c
Matthew Merrill <git@mattmerr.com>
2021-10-31 18:46:17 -0700

    Add itch promo command

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 module.exports = {
   DISCORD_TOKEN: process.env.DISCORD_TOKEN,
   SPREADSHEET_ID: process.env.SPREADSHEET_ID,
+  ITCH_URL: process.env.ITCH_URL,
   TAGS: [
     "feedback",
     "suggestion",
